2bc13324f842875eef17acc91dc1cc2502095f53
Shadiv <85450428+Shadiv@users.noreply.github.com>
2022-04-18 14:38:22 +0200

    FileSync
    filesync task 2

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import Filesync
 
 if __name__ == '__main__':
     file_sync = Filesync.Filesync()
-
+    with open('sync_log.log', 'w'):
+        pass
     while True:
         dirsync.sync(file_sync.source, file_sync.replica, 'sync', logger=file_sync.logger)
         time.sleep(file_sync.sleep)
